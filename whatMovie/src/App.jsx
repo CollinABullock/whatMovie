4d630451a81ec6d19c8939c29c87eeaa8cca11a6
Collin Bullock <collin@collinbullock.com>
2024-02-14 09:08:23 -0600

    mapping out the movies on the app to test the array

--- whatMovie/src/App.jsx
+++ whatMovie/src/App.jsx
@@ -6,6 +6,12 @@ function App() {
   return (
     <>
     <h1>What movie should you watch tonight?</h1>
+    {netflixArray.map((item, index) => (
+      <div>
+      <h1>{item.title}</h1>
+      <img src={item.poster} alt="This Film's Poster" />
+      </div>
+    ))}
     </>
   )
 }
