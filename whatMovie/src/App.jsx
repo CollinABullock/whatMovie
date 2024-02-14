148fb7c0c54b720184fd672b29e91f242f80e26b
Collin Bullock <collin@collinbullock.com>
2024-02-14 09:39:24 -0600

    added a random movie button to the home page

--- whatMovie/src/App.jsx
+++ whatMovie/src/App.jsx
@@ -1,20 +1,27 @@
 import { netflixArray } from './components/netflix'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [randomMovie, setRandomMovie] = useState(null);
+
+const handleRandomMovie = () => {
+  const randomIndex = Math.floor(Math.random() * netflixArray.length);
+  setRandomMovie(netflixArray[randomIndex]);
+}
 
   return (
     <>
     <h1>What movie should you watch tonight?</h1>
-    {netflixArray.map((item, index) => (
-      <div key={index}>
-      <h1>{item.title}</h1>
-      <img src={item.poster} alt="This Film's Poster" />
-      {item.actors.map ((actor, actorIndex) => (
-        <p key={actorIndex}>{actor.name}</p>
-      ))}
-      </div>
-    ))}
+    {randomMovie ? (
+  <div>
+  <h1>{randomMovie.title}</h1>
+  <img src={randomMovie.poster} alt="This Film's Poster" />
+  {randomMovie.actors.map((actor, actorIndex) => (
+    <p key={actorIndex}>{actor.name}</p>
+  ))}
+</div>
+    ) : (  <button onClick={handleRandomMovie}>Pick a random movie</button>)}
     </>
   )
 }
