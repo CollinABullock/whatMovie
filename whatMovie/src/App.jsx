a0049f30f3f1fbb5741b69f4bd92865e419fc1b7
Collin Bullock <collin@collinbullock.com>
2024-02-16 10:14:28 -0600

    trying to make this darn card work

--- whatMovie/src/App.jsx
+++ whatMovie/src/App.jsx
@@ -2,6 +2,7 @@ import { netflixArray } from './components/movieArray'
 import './App.css'
 import { useState } from 'react'
 import NavBar from './components/navbar';
+import Card from 'react-bootstrap/Card';
 
 function App() {
   const [randomMovie, setRandomMovie] = useState(null);
@@ -15,21 +16,20 @@ const handleRandomMovie = () => {
     <>
     <NavBar />
     <h1>What movie should you watch tonight?</h1>
+    
     {randomMovie ? (
-  <div>
-    <button onClick={handleRandomMovie}>I'm not feeling it, give me another</button>
-  <h1>{randomMovie.title}</h1>
-  <a href={randomMovie.link}>
-  <img src={randomMovie.poster} alt="This Film's Poster" /></a><br />
-
-  <h2>The Director: </h2><br />
-  {Array.isArray(randomMovie.director) ? ( // Check if director is an array
-      randomMovie.director.map((director, index) => (
-        <div key={index}>
-          <img src={director.image} alt="This person directed the movie" />
-          <p><a href={director.imdb}>{director.name}</a></p>
-        </div>
-      ))
+   <Card style={{ width: '18rem' }}>
+   <Card.Body>
+     <Card.Title>{randomMovie.title}</Card.Title>
+     <Card.Subtitle className="mb-2 text-muted">Directed by {randomMovie.director[0].name}</Card.Subtitle>
+     <Card.Text>
+       {randomMovie.description}
+     </Card.Text>
+     <Card.Link href="#">More Details</Card.Link>
+     <Card.Link onClick={handleRandomMovie}>I'm not feeling it, give me another</Card.Link>
+   </Card.Body>
+ </Card>
+      
     ) : (
       <div>
         <img src={randomMovie.director.image} alt="This person directed the movie" />
@@ -44,7 +44,7 @@ const handleRandomMovie = () => {
     <p key={actorIndex}><a href={actor.imdb}>{actor.name}</a></p>
     </>
   ))}
-</div>
+
     ) : (  <button onClick={handleRandomMovie}>Pick a random movie</button>)}
     </>
   )
