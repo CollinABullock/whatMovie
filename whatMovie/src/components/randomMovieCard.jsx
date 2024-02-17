f71f02d8a838c17537566d00f675b6d90d8e511d
“CollinABullock” <collin@collinbullock.com>
2024-02-17 13:00:23 -0600

    moved the Random Movie function into it's own component

---
+++ whatMovie/src/components/randomMovieCard.jsx
@@ -0,0 +1,40 @@
+import { netflixArray } from './movieArray'; 
+import { useState } from 'react'
+import Card from 'react-bootstrap/Card';
+
+export default function RandomMovie() {
+  const [randomMovie, setRandomMovie] = useState(null);
+
+const handleRandomMovie = () => {
+  const randomIndex = Math.floor(Math.random() * netflixArray.length);
+  setRandomMovie(netflixArray[randomIndex]);}
+
+  return (
+    <>
+      <div style={{marginTop: "20px"}}>
+    {randomMovie ? (
+   <Card style={{ width: "40%", marginTop: "20px", margin: "0 auto"}}>
+   <Card.Body>
+   <Card.Img src={randomMovie.poster} style={{width: "100%", height: "100%", objectFit: "cover"}} />
+     <Card.Title>{randomMovie.title}</Card.Title>
+     <Card.Text style={{textAlign: "start"}}>
+       {randomMovie.description}
+     </Card.Text>
+     <Card.Link href="#">More Details</Card.Link>
+     <Card.Link onClick={handleRandomMovie}>I'm not feeling it, give me another</Card.Link>
+   </Card.Body>
+ </Card>
+      
+
+    ) : (  
+    <>
+      <h1>What movie should you watch tonight?</h1>
+    <button onClick={handleRandomMovie}>Pick a random movie</button>
+    </>
+    )}
+    </div>
+    </>
+
+  )
+
+}
