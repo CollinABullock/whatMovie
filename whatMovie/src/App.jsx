09ea7aa334ea90eb6f0fd44b4812b4d825f7c988
“CollinABullock” <collin@collinbullock.com>
2024-02-19 11:47:49 -0600

    added preferences to main component to test them

--- whatMovie/src/App.jsx
+++ whatMovie/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import NavBar from './components/navbar';
 import RandomMovie from './components/randomMovieCard';
 import Footer from './components/footer';
+import Preferences from './components/Preferences';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
     <NavBar />
+    <Preferences />
     <RandomMovie />
     <Footer />
     </>
