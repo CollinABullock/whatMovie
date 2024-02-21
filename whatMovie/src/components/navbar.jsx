c05b3f8428cc129e85ac3cdef34c00a9e0ff16a7
Collin Bullock <collin@collinbullock.com>
2024-02-21 10:29:19 -0600

    added prevent default to navbar so that when dropdown menu indeed drops down it doesn't refresh

--- whatMovie/src/components/navbar.jsx
+++ whatMovie/src/components/navbar.jsx
@@ -5,6 +5,11 @@ import MoviePreferenceComponent from "./Preferences";
 export default function NavBar({ onPreferenceChange }) {
   const [showPreferences, setShowPreferences] = useState(false);
 
+  const handlePreferencesClick = (event) => {
+    event.preventDefault(); // Prevent the default anchor behavior
+    setShowPreferences(!showPreferences);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light fixed-top" style={{ padding: "10px", marginBottom: "20px", background: "transparent" }}>
       <a className="navbar-brand" style={{ color: "whitesmoke", fontSize: "1em" }}>Let's Choose a Damn Movie Already!</a>
@@ -15,7 +20,7 @@ export default function NavBar({ onPreferenceChange }) {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" onClick={() => setShowPreferences(!showPreferences)} aria-haspopup="true" aria-expanded="false" style={{ color: "whitesmoke" }}>
+            <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" onClick={handlePreferencesClick} aria-haspopup="true" aria-expanded="false" style={{ color: "whitesmoke" }}>
               Preferences
             </a>
             <div className={`dropdown-menu ${showPreferences ? 'show' : ''}`} aria-labelledby="navbarDropdown">
