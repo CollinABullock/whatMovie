d20e3a8f007b7ae8df186134890a79bcd8e5f49c
“CollinABullock” <collin@collinbullock.com>
2024-02-25 10:26:07 -0600

    fixed the passing down of props from preferences to navbar in the app jsx

--- whatMovie/src/App.jsx
+++ whatMovie/src/App.jsx
@@ -9,6 +9,20 @@ function App() {
   const [selectedRuntime, setSelectedRuntime] = useState(null);
   const [selectedGenres, setSelectedGenres] = useState(null);
   const [preferredGenres, setPreferredGenres] = useState([]); // Change to an empty array
+  const [uniqueGenres, setUniqueGenres] = useState([]); // Add uniqueGenres state
+
+  useEffect(() => {
+    const genresSet = new Set();
+    moviesArray.forEach(movie => {
+      if (movie.genre) {
+        movie.genre.forEach(genre => {
+          genresSet.add(genre);
+        });
+      }
+    });
+    const sortedGenres = Array.from(genresSet).sort();
+    setUniqueGenres(sortedGenres);
+  }, []);
 
   const handlePreferenceChange = (runtime, genres, preferredGenres) => {
     setSelectedRuntime(runtime);
@@ -18,7 +32,11 @@ function App() {
 
   return (
     <>
-      <NavBar data={moviesArray} onPreferenceChange={handlePreferenceChange} />
+      <NavBar
+        data={moviesArray}
+        onPreferenceChange={handlePreferenceChange}
+        uniqueGenres={uniqueGenres} // Pass uniqueGenres
+      />
       <div className="container">
         <RandomMovie selectedRuntime={selectedRuntime} selectedGenres={selectedGenres} preferredGenres={preferredGenres} /> {/* Pass preferredGenres */}
       </div>
--- whatMovie/src/components/navbar.jsx
+++ whatMovie/src/components/navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import MoviePreferenceComponent from "./Preferences";
-import { netflixArray } from './movieArray';
+import { moviesArray, netflixArray } from './movieArray';
 
 export default function NavBar({ onPreferenceChange, uniqueGenres }) {
   const [showPreferences, setShowPreferences] = useState(false);
@@ -14,7 +14,7 @@ export default function NavBar({ onPreferenceChange, uniqueGenres }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light fixed-top" style={{ padding: "10px", marginBottom: "20px", background: "transparent" }}>
       <a className="navbar-brand" style={{ color: "whitesmoke", fontSize: "1em" }}>Let's Choose a Damn Movie Already!</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style={{backgroundColor: "white"}}>
         <span className="navbar-toggler-icon"></span>
       </button>
 
